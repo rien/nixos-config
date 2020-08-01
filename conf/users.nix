e188e8954ad2180c5a67e4ece267e364c3f38317
Rien Maertens <rien.maertens@posteo.be>
2020-08-01 18:36:22 +0200

    Extract public key, s to unencrypted file

--- conf/users.nix
+++ conf/users.nix
@@ -1,13 +1,16 @@
 { ... }:
 let
   personal = import ./personal.secret.nix;
+  sshkeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKBB/PjWusmMRRWdhSIMmrA/6s6hESBKVdvo6S26LUh1"
+  ];
 in
 {
-  users.users.root.openssh.authorizedKeys.keys = personal.sshkeys;
+  users.users.root.openssh.authorizedKeys.keys = sshkeys;
   users.users.rien = {
     isNormalUser = true;
     createHome = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    openssh.authorizedKeys.keys = personal.sshkeys;
+    openssh.authorizedKeys.keys = sshkeys;
   };
 }
