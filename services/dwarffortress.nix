ca8788e98756ba3f82e773fc1646469cb42c28f2
Rien Maertens <rien.maertens@posteo.be>
2020-10-24 22:42:49 +0200

    Urgh jails

--- services/dwarffortress.nix
+++ services/dwarffortress.nix
@@ -13,6 +13,8 @@ in
       AllowTcpForwarding no
   '';
 
+  # programs.firejail.enable = true;
+
   nixpkgs.config.allowUnfree = true;
   users.users."${username}" = {
     isNormalUser = true;
@@ -20,6 +22,7 @@ in
     openssh.authorizedKeys.keys = keys;
     shell =
       let
+        # firejail = "${pkgs.firejail}/bin/firejail -c --noprofile --net=none --shell=none --";
         abduco = "${pkgs.abduco}/bin/abduco -lA df";
         df = "${pkgs.dwarf-fortress.override { enableSound = false; enableTextMode = true; }}/bin/dwarf-fortress";
         df-shell = pkgs.writeShellScriptBin "df-shell" "${abduco} ${df}";
