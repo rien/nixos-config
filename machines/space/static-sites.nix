0807a8ff36fc1a85a0f8988a002871fc3655525a
Rien Maertens <rien.maertens@posteo.be>
2020-10-26 21:38:39 +0100

    More sites

--- machines/space/static-sites.nix
+++ machines/space/static-sites.nix
@@ -1,5 +1,12 @@
 { ... }:
 {
+  users.users.webhost = {
+    isNormalUser = true;
+    createHome = true;
+    home = /srv/webhost;
+    openssh.authorizedKeys.keys = [ (import ../../conf/personal.secret.nix).pcKey ];
+  };
+
   services.nginx = {
     virtualHosts = {
       "space.rxn.be" = {
@@ -11,6 +18,31 @@
           return = "200 \"\\n███████╗    ██████╗      █████╗      ██████╗    ███████╗\\n██╔════╝    ██╔══██╗    ██╔══██╗    ██╔════╝    ██╔════╝\\n███████╗    ██████╔╝    ███████║    ██║         █████╗\\n╚════██║    ██╔═══╝     ██╔══██║    ██║         ██╔══╝\\n███████║    ██║         ██║  ██║    ╚██████╗    ███████╗\\n╚══════╝    ╚═╝         ╚═╝  ╚═╝     ╚═════╝    ╚══════╝\\n\"";
         };
       };
+      "rien.maertens.io" = {
+        enableACME = true;
+        forceSSL = true;
+        root = "/srv/webhost/maertens.io";
+      };
+      "maertens.io" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          return = "302 https://rien.maertens.io";
+        };
+      };
+      "theatervolta.be" = {
+        serverAliases = [ "www.theatervolta.be" "voltaprojects.be" "www.voltaprojects.be" ];
+        enableACME = true;
+        # useACMEHost = "theatervolta.be";
+        forceSSL = true;
+        root = "/srv/webhost/volta";
+      };
     };
   };
+
+
+  #security.acme.certs."theatervolta.be" = {
+  #  dnsProvider = "hetzner";
+  #  credentialsFile = "/etc/nixos/secrets/acme-credentials";
+  #};
 }
