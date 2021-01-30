1a3a937fec4dfd9261f7fc82bcff70f28d1d7216
Rien Maertens <rien.maertens@posteo.be>
2021-01-31 00:59:46 +0100

    Fix vpnc module

--- modules/vpnc/default.nix
+++ modules/vpnc/default.nix
@@ -2,15 +2,6 @@
 with lib;
 let
   cfg = config.custom.vpnc;
-  vpnc = pkgs.vpnc;
-  vu = pkgs.writeScriptBin "vu"
-  ''
-    sudo ${vpnc}/bin/vpnc --script ${vpnc}/etc/vpnc/vpnc-script /run/secrets/vpnc
-  '';
-  vd = pkgs.writeScriptBin "vd"
-  ''
-    sudo ${vpnc}/bin/vpnc-disconnect
-  '';
 in {
   options.custom.vpnc.enable = mkOption {
     default = false;
@@ -19,8 +10,32 @@ in {
 
   config = mkIf cfg.enable {
     age.secrets."vpnc".file = ./vpnc.age;
-    home-manager.users.rien = { ... }: {
-      home.packages = [ vu vd ];
+
+    systemd.services.vpnc = let
+      vpnc = pkgs.vpnc;
+      script = pkgs.writeScript "vpnc-script"
+      ''
+        export INTERNAL_IP4_NETADDR=157.193.0.0
+        export INTERNAL_IP4_NETMASK=255.255.0.0
+        export INTERNAL_IP4_NETMASKLEN=16
+        exec ${vpnc}/etc/vpnc/vpnc-script
+      '';
+    in {
+      unitConfig = {
+        Description = "VPNC Connection";
+        After = "network.target";
+      };
+
+      serviceConfig = {
+        Type = "forking";
+        ExecStart = "${vpnc}/bin/vpnc --pid-file=/run/vpnc.pid --script ${script} /run/secrets/vpnc";
+
+        PIDFile = "/run/vpnc.pid";
+        Restart = "always";
+
+      };
+
+      path = [ pkgs.nettools ];
     };
   };
 }
