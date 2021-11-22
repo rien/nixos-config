81c0d00c065ebed541f99f3d71018443c7c6447d
Rien Maertens <rien.maertens@posteo.be>
2021-11-22 10:08:17 +0100

    Random changes which don't work

--- machines/living/default.nix
+++ machines/living/default.nix
@@ -7,38 +7,21 @@
       ./hardware-configuration.nix
     ];
 
-  age.secrets."media-key".file = ./living_key.age;
-
-  boot.kernel.sysctl = {
-    "fs.inotify.max_user_watches" = 32768;
-  };
-
-
   custom = {
     bash.enable = true;
-    neovim.enable = true;
-    mounts.media = {
-      enable = true;
-      identityFile = "/run/secrets/media-key";
-    };
+  };
 
-    extraSystemPackages = with pkgs; [
-      mpv
-      ffmpeg
-    ];
-
-    minidlna = {
-      enable = true;
-      dirs = [ "/media" ];
-    };
+  wireless = {
+    enable = true;
+    device = "wlan0";
   };
 
   documentation.enable = false;
-  networking.hostName = "living"; # Define your hostname.
+  networking.hostName = "harmony"; # Define your hostname.
 
   # Don't change this.
   # See https://nixos.org/manual/nixos/stable/options.html#opt-system.stateVersion
-  system.stateVersion = "21.03";
+  system.stateVersion = "21.11";
 
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
--- machines/living/hardware-configuration.nix
+++ machines/living/hardware-configuration.nix
@@ -21,12 +21,6 @@
     version = 3;
   };
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.tcp.enable = true;
-  hardware.pulseaudio.tcp.anonymousClients.allowedIpRanges = [ "127.0.0.1" "10.0.0.0/24" ];
-  networking.firewall.allowedTCPPorts = [ 4713 ];
-
   environment.systemPackages = with pkgs; [
     libraspberrypi
     raspberrypifw
@@ -38,13 +32,6 @@
     device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
     fsType = "ext4";
   };
-
-  swapDevices = [
-    {
-      device = "/swapfile";
-      size = 2048;
-    }
-  ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 
