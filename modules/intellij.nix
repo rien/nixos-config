6d6a589c9d15f5031cc45da9b5749e9f360318ca
Rien Maertens <rien.maertens@posteo.be>
2022-09-28 15:14:17 +0200

    Fix GitHub copilot

--- modules/intellij.nix
+++ modules/intellij.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, custompkgs, ... }:
+{ stdenv, config, lib, pkgs, custompkgs, ... }:
 let
   cfg = config.custom.intellij;
 in {
@@ -30,11 +30,65 @@ in {
         valgrind = valgrind;
       };
       extraPath = lib.makeBinPath (builtins.attrValues devSDKs);
+      # Copilot
+      addCopilot = (editor:
+        with pkgs.jetbrains.plugins;
+        let
+          info = (getUrl { id = "17718"; hash = "sha256-YttM/ueluqfvYsU93Ebov9rTCL4vWcUppipfnU87ZpM="; });
+          libPath = lib.makeLibraryPath [pkgs.glibc pkgs.gcc-unwrapped];
+          copilot-plugin = (urlToDrv {
+            name = "GitHub Copilot";
+            url = info.url;
+            hash = "sha256-117CHiwMOlEoiZBRk7hT3INncargoeYCuewpCeQ4nz8=";
+            extra = {
+              inputs = [ pkgs.patchelf pkgs.glibc pkgs.gcc-unwrapped ];
+              commands = ''
+                agent="copilot-agent/bin/copilot-agent-linux"
+                orig_size=$(stat --printf=%s $agent)
+                patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $agent
+                patchelf --set-rpath ${libPath} $agent
+                chmod +x $agent
+                new_size=$(stat --printf=%s $agent)
+
+                # https://github.com/NixOS/nixpkgs/pull/48193/files#diff-329ce6280c48eac47275b02077a2fc62R25
+                ###### zeit-pkg fixing starts here.
+                # we're replacing plaintext js code that looks like
+                # PAYLOAD_POSITION = '1234                  ' | 0
+                # [...]
+                # PRELUDE_POSITION = '1234                  ' | 0
+                # ^-----20-chars-----^^------22-chars------^
+                # ^-- grep points here
+                #
+                # var_* are as described above
+                # shift_by seems to be safe so long as all patchelf adjustments occur 
+                # before any locations pointed to by hardcoded offsets
+
+                var_skip=20
+                var_select=22
+                shift_by=$(expr $new_size - $orig_size)
+                function fix_offset {
+                  # $1 = name of variable to adjust
+                  location=$(grep -obUam1 "$1" $agent | cut -d: -f1)
+                  location=$(expr $location + $var_skip)
+
+                  value=$(dd if=$agent iflag=count_bytes,skip_bytes skip=$location \
+                   bs=1 count=$var_select status=none)
+                  value=$(expr $shift_by + $value)
+
+                  echo -n $value | dd of=$agent bs=1 seek=$location conv=notrunc
+                }
+                fix_offset PAYLOAD_POSITION
+                fix_offset PRELUDE_POSITION
+              '';
+            };
+          });
+        in addPlugins editor [ copilot-plugin ]);
+      idea-with-copilot = addCopilot pkgs.jetbrains.idea-ultimate;
       intellij = pkgs.runCommand "intellij"
         { nativeBuildInputs = [ pkgs.makeWrapper ]; }
         ''
           mkdir -p $out/bin
-          makeWrapper ${pkgs.jetbrains.idea-ultimate}/bin/idea-ultimate \
+          makeWrapper ${idea-with-copilot}/bin/idea-ultimate \
             $out/bin/intellij \
             --prefix PATH : ${extraPath}
         '';
